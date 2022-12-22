79e43c18c4acb10c6de430c5eefa3264ef91b9a5
Popov Roman <popovroman1@gmail.com>
2022-12-22 20:06:13 +0300

    Добавили HTML разметку,  создали форму добавления записей

--- my_first_blog/blog_writing/urls.py
+++ my_first_blog/blog_writing/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path('', views.PostView.as_view()),
-               path('<int:pk>/', views.PostDetail.as_view())]
+urlpatterns = [path('', views.PostView.as_view(), name='home'),
+               path('<int:pk>/', views.PostDetail.as_view()),
+               path('about_us', views.about_us, name='about'),
+               path('create', views.create, name='create'),
+
+               ]
--- my_first_blog/blog_writing/views.py
+++ my_first_blog/blog_writing/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.base import View
 from .models import Post
+from .forms import PostForms
+
 
 class PostView(View):
     '''Вывод записей'''
+
     def get(self, request):
         posts = Post.objects.all()
         return render(request, 'blog/blog.html', {'post_list': posts})
@@ -11,6 +14,31 @@ class PostView(View):
 
 class PostDetail(View):
     '''Вывод записей на отдельной странице'''
+
     def get(self, request, pk):
         post = Post.objects.get(id=pk)
         return render(request, 'blog/blog_detail.html', {'post': post})
+
+
+def about_us(request):  # Вывод информации о нас
+    return render(request, 'blog/about.html')
+
+
+def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = PostForms(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Форма неверное заполнена'
+
+    form = PostForms()
+
+    data = {
+        'form': form,
+        'error': error
+    }
+
+    return render(request, 'blog/create.html', data)
